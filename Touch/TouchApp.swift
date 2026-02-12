5da0d4719d2c2de08c27f7813203cfb180aa324d
borisgraudt <boris.graudt@gmail.com>
2026-02-12 12:53:31 +0300

    Initial Commit

---
+++ Touch/TouchApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchApp.swift
+//  Touch
+//
+//  Created by boris on 2/12/26.
+//
+
+import SwiftUI
+
+@main
+struct TouchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TouchTests/TouchTests.swift
@@ -0,0 +1,17 @@
+//
+//  TouchTests.swift
+//  TouchTests
+//
+//  Created by boris on 2/12/26.
+//
+
+import Testing
+@testable import Touch
+
+struct TouchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
